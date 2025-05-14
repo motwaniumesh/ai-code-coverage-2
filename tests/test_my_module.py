b94e2a5c8aa060d4fd48bdde0ff7869926c6702c
Umesh Motwani <umesh.motwani@slalom.com>
2025-05-14 16:22:20 -0400

    add

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,9 +1,9 @@
 import pytest
 from src.my_module import add, is_even
 
-# def test_add():
-#     assert add(2, 3) == 5
-#     assert add(-1, 1) == 0
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
 
 # def test_is_even():
 #     assert is_even(4) is True
